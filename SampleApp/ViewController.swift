92ff74fd93ddb7462198f2d4109aa44899b82385
Mario Louis Vincent Villamor <mario@KKs-MacBook-Pro.local>
2020-06-03 15:58:21 +0800

    Init Commit

---
+++ SampleApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleApp
+//
+//  Created by Mario Louis Vincent Villamor on 03/06/2020.
+//  Copyright © 2020 Mario Louis Vincent Villamor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
